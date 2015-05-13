7a52c8e92a6eab6ae1dba4e701f92764b8f5d3ff
Nick Bonnet <njbuwm@gmail.com>
2015-05-12 21:03:02 -0400

    Fixed constant

--- app/scripts/services/taskService.js
+++ app/scripts/services/taskService.js
@@ -9,12 +9,12 @@
  */
 angular.module('toDueApp')
 .constant('dataURL', 'http://localhost:5500/todos')
-.factory('TaskService', ['$http', function($http, dataURL){
+.factory('TaskService', ['$http', 'dataURL', function($http, dataURL){
     var service = {};
 
     service.getTasks = function() {
     	var result = 
-    	$http.get('http://localhost:5500/todos') // NEED TO FIX: Why won't it get datURL
+    	$http.get(dataURL) // NEED TO FIX: Why won't it get datURL
         .success(function (data) {
             result.data = data;
             return result;
